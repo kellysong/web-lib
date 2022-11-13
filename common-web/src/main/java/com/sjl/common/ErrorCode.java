cb82adaa09e1cc9caff51627f3f44a72850730f4
kellysong <488818949@qq.com>
2022-11-13 16:54:50 +0800

    完善错误码

--- common-web/src/main/java/com/sjl/common/ErrorCode.java
+++ common-web/src/main/java/com/sjl/common/ErrorCode.java
@@ -9,45 +9,55 @@ public interface ErrorCode {
 	/**
 	 * 请求成功
 	 */
-	public static final int ERROR_OK = 0;
+	 int ERROR_OK = 0;
 	/**
 	 * 请求没有数据或者没有资源，或者资源已经是最新了
 	 */
-	public static final int ERROR_NO_DATA = -1;
+	 int ERROR_NO_DATA = -1;
 	
 	/**
 	 * 数据已存在
 	 */
-	public static final int ERROR_DATA_EXIST = -2;
+	 int ERROR_DATA_EXIST = -2;
 	/**
 	 * 请求参数为空
 	 */
-	public static final int ERROR_NULL_PARAMETER = -3;
+	 int ERROR_NULL_PARAMETER = -3;
 	/**
 	 * 请求参数无效
 	 */
-	public static final int ERROR_INVALID_PARAMETER = -4;
+	 int ERROR_INVALID_PARAMETER = -4;
 	
 	/**
 	 * 请求超时
 	 */
-	public static final int ERROR_TIMEOUT = -5;
+	 int ERROR_TIMEOUT = -5;
 
 	/**
 	 * 请求没有权限
 	 */
-	public static final int ERROR_NO_ACCESS = -6;
+	 int ERROR_NO_ACCESS = -6;
 
 	/**
 	 * 文件已经移除
 	 */
-	public static final int ERROR_FILE_DELETE = -7;
-	
-	
+	 int ERROR_FILE_DELETE = -7;
 
+
+	/**
+	 * 存在绑定的数据，无法操作
+	 */
+	 int ERROR_EXIST_BIND_DATA = -8;
+
+
+	/**
+	 * 修改的记录不存在
+	 */
+	 int ERROR_UPDATE_NO_RECORD = -9;
+	
 	/**
 	 * 请求失败或者异常
 	 */
-	public static final int ERROR_FAIL = -9;
+	 int ERROR_FAIL = -99;
 
 }
